009a5f3f6ad0f80e2ac6d28dfced3ffaf8b03d09
Abram Phillips <abramcpg@hotmail.com>
2019-06-17 12:06:02 -0400

    99 passed
    unfinished:
    StringUtils
    StringArrayUtils
    Address

--- src/main/java/rocks/zipcode/assessment2/objectorientation/Address.java
+++ src/main/java/rocks/zipcode/assessment2/objectorientation/Address.java
@@ -14,46 +14,68 @@ public class Address {
      * @param state - state of city
      * @param zipcode - zipcode of region
      */
+    String addressLine1 ="";
+    String addressLine2="";
+    String city="";
+    String state="";
+    String zipcode="";
+
     public Address(String addressLine1, String addressLine2, String city, String state, String zipcode) {
+        this.addressLine1 = addressLine1;
+        this.addressLine2 = addressLine2;
+        this.city = city;
+        this.state = state;
+        this.zipcode = zipcode;
     }
 
     public String getAddressLine1() {
-        return null;
+        return addressLine1;
     }
 
     public void setAddressLine1(String addressLine1) {
+        this.addressLine1 = addressLine1;
     }
 
     public String getAddressLine2() {
-        return null;
+        return addressLine2;
     }
 
     public void setAddressLine2(String addressLine2) {
+        this.addressLine2 = addressLine2;
     }
 
     public String getCity() {
-        return null;
+        return this.city;
     }
 
     public void setCity(String city) {
+        this.city =city;
     }
 
     public String getState() {
-        return null;
+        return state;
     }
 
     public void setState(String state) {
+        this.state = state;
     }
 
     public String getZipcode() {
-        return null;
+        return zipcode;
     }
 
     public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
     }
 
+    public String toString(String expected, Address address){
+        String newString = String.format("Address{addressline1='%s', addressLine2='%s', city='%s', state='%s', zipcode='%s'}",addressLine1,addressLine2,city,state,zipcode);
+        String answer = "test";
+        return answer;
+    }
     @Override
     public boolean equals(Object o) {
-        return (Boolean)null;
+
+        return (addressLine2 == addressLine1);
     }
 }
