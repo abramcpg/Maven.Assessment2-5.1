65d428819b666a2531a70315310c5884578d984a
Abram Phillips <abramcpg@hotmail.com>
2019-06-17 12:25:28 -0400

    104 passed

--- src/main/java/rocks/zipcode/assessment2/objectorientation/Person.java
+++ src/main/java/rocks/zipcode/assessment2/objectorientation/Person.java
@@ -1,5 +1,7 @@
 package rocks.zipcode.assessment2.objectorientation;
 
+import static java.lang.Long.MIN_VALUE;
+
 /**
  * @author leon on 28/11/2018.
  * @ATTENTION_TO_STUDENTS - Ensure that you have completed the `Address` class before attempting this class
@@ -8,33 +10,47 @@ public class Person {
     /**
      * @param id - id of person
      * @param name - name of person
-     * @param address - address of person
+     *@param address - address of person
      */
-    public Person(Long id, String name, Address address) {
+    private  Person person;
+    private Long id = MIN_VALUE;
+    private String name = "";
+    private Address address = new Address();
+
+    public Person(Long id, String name, Address address) { //constructor
+        this.id = id;
+        this.name = name;
+        this.address = address;
     }
 
     public Person() {
+        this.id = id;
+        this.name = name;
+        this.address = address;
     }
 
     public Long getId() {
-        return null;
+        return this.id;
     }
 
     public void setId(Long id) {
+        this.id = id;
     }
 
     public String getName() {
-        return null;
+        return name;
     }
 
     public void setName(String name) {
+        this.name = name;
     }
 
     public Address getAddress() {
-        return null;
+        return address;
     }
 
     public void setAddress(Address address) {
+        this.address = address;
     }
 
     @Override
